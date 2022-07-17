ac7ced7c1b46238e87ffd2c111084cefdd6bc2a7
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-07-17 11:36:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logoSergio from './imagenes/Sergio Sánchez.png'
+import { useState } from 'react'
+
 
 function App() {
+
+const [numClics, setNumClics] = useState(0);
+
+const manejarClic = ()=> {
+setNumClics(numClics + 1);
+}
+
+const reiniciarContador = () =>{
+setNumClics(0);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='logo'>
+       <img 
+       className='logo-imagen'
+       src={logoSergio}
+       alt='Logo de Sergio'/>
+     </div>
+     <div className='contenedor-contador'>
+       <Contador numClics={numClics} />
+      <Boton
+      texto='Clic'
+      esBotonDeClic={true}
+      manejarClic={manejarClic} />
+
+      <Boton
+      texto='Reiniciar'
+      esBotonDeClic={false}
+      manejarClic={reiniciarContador} />
+     </div>
     </div>
   );
 }
